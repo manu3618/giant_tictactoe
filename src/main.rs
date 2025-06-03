217c9d7be6ca81d96486e5b3cdbfa7cc44a09e13
manu <manu+github@hbrt.eu>
2025-06-03 17:34:06 +0200

    Add victories

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,13 @@ impl fmt::Display for Space {
         write!(f, "{}", d)
     }
 }
+
+impl From<Space> for char {
+    fn from(s: Space) -> Self {
+        format!("{}", s).chars().next().unwrap()
+    }
+}
+
 #[derive(Debug, Default, Clone)]
 struct TicTacToe {
     grid: [[Space; 3]; 3],
@@ -61,6 +68,34 @@ impl TicTacToe {
         *&mut self.grid[coords.0][coords.1] = player.clone();
         Ok(())
     }
+
+    fn victory(&self) -> Space {
+        for p in [Space::Cross, Space::Circle] {
+            for c in 0..3 {
+                if (0..3).map(|u| &self.grid[c][u]).cloned().all(|x| x == p) {
+                    // line
+                    return p;
+                }
+                if (0..3).map(|u| &self.grid[u][c]).cloned().all(|x| x == p) {
+                    // column
+                    return p;
+                }
+            }
+            if (0..3).map(|u| &self.grid[u][u]).cloned().all(|x| x == p) {
+                // first diag
+                return p;
+            }
+            if (0..3)
+                .map(|u| &self.grid[u][2 - u])
+                .cloned()
+                .all(|x| x == p)
+            {
+                // second diag
+                return p;
+            }
+        }
+        Space::None
+    }
 }
 
 #[derive(Debug, Default)]
@@ -88,8 +123,17 @@ impl GiantTicTacToe {
     fn to_grid(&self, playable: Option<usize>) -> Result<String, String> {
         // grids
         let mut grid = empty_giant_grid();
+
         // data
-        // TODO
+        for (l, x) in self.grid.iter().enumerate() {
+            for (c, g) in x.iter().enumerate() {
+                let grid_offset = (l * 12 + 1, c * 12 + 1);
+                for (line, column) in iproduct!(0..3, 0..3) {
+                    let coords = (4 * line + grid_offset.0, 4 * column + grid_offset.1);
+                    grid[coords.0][coords.1] = g.grid[line][column].clone().into();
+                }
+            }
+        }
 
         // numbers
         if let Some(g) = playable {
@@ -112,18 +156,32 @@ impl GiantTicTacToe {
         let result = lines.join("\n");
         Ok(result)
     }
+
+    fn victories(&self) -> TicTacToe {
+        let mut t = TicTacToe::default();
+        for (a, b) in iproduct!(0..3, 0..3) {
+            t.grid[a][b] = self.grid[a][b].victory();
+        }
+        t
+    }
+
+    fn victory(&self) -> Space {
+        self.victories().victory()
+    }
 }
 
 fn empty_giant_grid() -> [[char; 3 * 12]; 3 * 12] {
     let mut grid = [[' '; 3 * 12]; 3 * 12];
     for (x, y) in iproduct!(0..3, 0..3) {
         let offset = (x * 12, y * 12);
+
         // lines
         for l in 0..2 {
             for c in 0..11 {
                 grid[3 + l * 4 + offset.0][c + offset.1] = '-'
             }
         }
+
         // columns
         for c in 0..2 {
             for l in 0..11 {
